2a3d805f416e080b525ed4c58f06904ac8f998bf
KruszewskiD <daniel.patryk.kruszewski@gmail.com>
2024-02-02 06:45:21 +0100

    Fixed: Change UseEffect to UseFocusEffect in RecentlyWatchedScreeb

--- CineLog/src/screens/RecentlyWatchedScreen.jsx
+++ CineLog/src/screens/RecentlyWatchedScreen.jsx
@@ -1,24 +1,33 @@
 import {View} from 'react-native';
 import VerticalMovieList from '../components/VerticalMovieList';
 import {POPULAR} from '../constants/popular';
-import {useEffect, useState} from 'react';
+import {useCallback, useEffect, useState} from 'react';
 import {updateAsyncStorage} from '../asyncStorage/updateAsyncStorage';
+import {useFocusEffect} from '@react-navigation/native';
 const RecentlyWatchedScreen = () => {
   const [asyncMovieData, setAsyncMovieData] = useState();
 
-  useEffect(() => {
-    // updateAsyncStorage('clearStorage');
-    const getRecentlyWatchedData = async key => {
-      try {
-        const fetchedData = await updateAsyncStorage(key);
-        setAsyncMovieData(await fetchedData);
-      } catch (error) {
-        console.error('Failed to fetch or set movie data:', error);
-      }
-    };
+  useFocusEffect(
+    useCallback(() => {
+      let isActive = true;
 
-    getRecentlyWatchedData('watched-movies');
-  }, []);
+      const getRecentlyWatchedData = async key => {
+        try {
+          const fetchedData = await updateAsyncStorage(key);
+          setAsyncMovieData(await fetchedData);
+        } catch (error) {
+          console.error('Failed to fetch or set movie data:', error);
+        }
+      };
+
+      getRecentlyWatchedData('watched-movies');
+
+      return () => {
+        isActive = false;
+      };
+    }, []),
+  );
+
   return (
     <View
       style={{
